c69ace2e9deb3dc8b4af6549689dfc9e433fb8b3
marcus <linfaimom@gmail.com>
2016-04-17 16:06:06 +0800

    Update README.md

---
+++ app/src/main/java/com/example/marcus/knowYou/LoadActivity.java
@@ -0,0 +1,29 @@
+package com.example.marcus.knowYou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+/**
+ * Created by marcus on 16/4/16.
+ */
+public class LoadActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+        setContentView(R.layout.load);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+    }
+}
--- app/src/main/java/com/example/marcus/knowYou/ViewPagerFragment/Foods.java
+++ app/src/main/java/com/example/marcus/knowYou/ViewPagerFragment/Foods.java
@@ -6,16 +6,67 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.marcus.knowYou.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by marcus on 16/4/13.
  */
 public class Foods extends Fragment {
+    private ListView foodsList;
+    private List<Map<String,String>> list;
+    private int[] pics = new int[]{};
+    private String[][] infos = new String[][]{
+            {"七里香","123"},
+            {"扬州风味","123"},
+            {"巴比汉堡","123"},
+            {"东吴人家","123"},
+            {"街7中式快餐","123"},
+            {"东吴人家","123"},
+            {"台湾烤鱼","123"},
+            {"正宗韩国快餐","123"},
+            {"麦尚堡","123"},
+            {"贝壳汉堡","123"}
+    };
+    private SimpleAdapter adapter;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.foods,null);
+        View view = inflater.inflate(R.layout.foods,null);
+        foodsList = (ListView) view.findViewById(R.id.foodsList);
+        initList();
+        setAdapter();
+        return view;
     }
+
+    private void initList() {
+        list = new ArrayList<>();
+        for (int i=0; i<infos.length; i++){
+            Map<String,String> map = new HashMap<>();
+            map.put("pic",String.valueOf(R.drawable.girl));
+            map.put("title",infos[i][0]);
+            map.put("info",infos[i][1]);
+            map.put("star",String.valueOf(R.drawable.clothes));
+            map.put("type","浙师大北门");
+            list.add(map);
+        }
+    }
+
+    private void setAdapter() {
+        adapter = new SimpleAdapter(getActivity(),list,R.layout.foods_listview,
+                new String[]{"pic","title","info","star","type"},
+                new int[]{R.id.pic,R.id.title,R.id.info,R.id.star,R.id.type});
+        foodsList.setAdapter(adapter);
+    }
+
+
 }
